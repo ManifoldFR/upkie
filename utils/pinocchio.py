d611d66f2eea9e9470a737764b2024a7127264dd
ManifoldFR <wilson.jallet@polytechnique.org>
2023-06-28 17:23:37 +0200

    [utils/pinocchio] add create_cartpole function
    + fix some issues raised by Ruff for pydocstyle
    + set black line-length config

--- utils/pinocchio.py
+++ utils/pinocchio.py
@@ -19,11 +19,11 @@ from typing import Tuple
 
 import numpy as np
 import pinocchio as pin
+import hppfcl as fcl
 
 
 def box_position_limits(model: pin.Model) -> Tuple[np.ndarray, np.ndarray]:
-    r"""!
-    Compute position limits in box format:
+    r"""!Compute position limits in box format:
 
     \f[
     q_{min} \leq q \leq q_{max}
@@ -45,8 +45,7 @@ def box_position_limits(model: pin.Model) -> Tuple[np.ndarray, np.ndarray]:
 
 
 def box_velocity_limits(model: pin.Model) -> Tuple[np.ndarray, np.ndarray]:
-    r"""!
-    Compute velocity limits in box format:
+    r"""!Compute velocity limits in box format:
 
     \f[
     v_{min} \leq v \leq v_{max}
@@ -66,8 +65,7 @@ def box_velocity_limits(model: pin.Model) -> Tuple[np.ndarray, np.ndarray]:
 
 
 def box_torque_limits(model: pin.Model) -> Tuple[np.ndarray, np.ndarray]:
-    r"""!
-    Compute velocity limits in box format:
+    r"""!Compute velocity limits in box format:
 
     \f[
     \tau_{min} \leq \tau \leq \tau_{max}
@@ -84,3 +82,91 @@ def box_torque_limits(model: pin.Model) -> Tuple[np.ndarray, np.ndarray]:
     tau_max = model.effortLimit.copy()
     tau_max[no_torque_limit] = np.inf
     return tau_max
+
+
+def create_cartpole(N) -> Tuple[pin.Model, pin.GeometryModel]:
+    """Create a cartpole Pinocchio model."""
+    model = pin.Model()
+    geom_model = pin.GeometryModel()
+
+    parent_id = 0
+
+    cart_radius = 0.1
+    cart_length = 5 * cart_radius
+    cart_mass = 1.0
+    joint_name = "joint_cart"
+
+    geometry_placement = pin.SE3.Identity()
+    geometry_placement.rotation = pin.Quaternion(
+        np.array([0.0, 0.0, 1.0]), np.array([0.0, 1.0, 0.0])
+    ).toRotationMatrix()
+
+    joint_id = model.addJoint(
+        parent_id, pin.JointModelPY(), pin.SE3.Identity(), joint_name
+    )
+
+    body_inertia = pin.Inertia.FromCylinder(
+        cart_mass, cart_radius, cart_length
+    )
+    body_placement = geometry_placement
+    model.appendBodyToJoint(joint_id, body_inertia, body_placement)
+    # We need to rotate the inertia as it is expressed in the LOCAL
+    # frame of the geometry
+
+    shape_cart = fcl.Cylinder(cart_radius, cart_length)
+
+    geom_cart = pin.GeometryObject(
+        "shape_cart", joint_id, geometry_placement, shape_cart
+    )
+    geom_cart.meshColor = np.array([1.0, 0.1, 0.1, 1.0])
+    geom_model.addGeometryObject(geom_cart)
+
+    parent_id = joint_id
+    joint_placement = pin.SE3.Identity()
+    body_mass = 0.1
+    body_radius = 0.1
+    for k in range(N):
+        joint_name = "joint_" + str(k + 1)
+        joint_id = model.addJoint(
+            parent_id, pin.JointModelRX(), joint_placement, joint_name
+        )
+
+        body_inertia = pin.Inertia.FromSphere(body_mass, body_radius)
+        body_placement = joint_placement.copy()
+        body_placement.translation[2] = 1.0
+        model.appendBodyToJoint(joint_id, body_inertia, body_placement)
+
+        geom1_name = "ball_" + str(k + 1)
+        shape1 = fcl.Sphere(body_radius)
+        geom1_obj = pin.GeometryObject(
+            geom1_name, joint_id, body_placement, shape1
+        )
+        geom1_obj.meshColor = np.ones((4))
+        geom_model.addGeometryObject(geom1_obj)
+
+        geom2_name = "bar_" + str(k + 1)
+        shape2 = fcl.Cylinder(body_radius / 4.0, body_placement.translation[2])
+        shape2_placement = body_placement.copy()
+        shape2_placement.translation[2] /= 2.0
+
+        geom2_obj = pin.GeometryObject(
+            geom2_name, joint_id, shape2_placement, shape2
+        )
+        geom2_obj.meshColor = np.array([0.0, 0.0, 0.0, 1.0])
+        geom_model.addGeometryObject(geom2_obj)
+
+        parent_id = joint_id
+        joint_placement = body_placement.copy()
+    end_frame = pin.Frame(
+        "end_effector_frame",
+        model.getJointId("joint_" + str(N)),
+        0,
+        body_placement,
+        pin.FrameType(3),
+    )
+    model.addFrame(end_frame)
+    geom_model.collision_pairs = []
+    model.qinit = np.zeros(model.nq)
+    model.qinit[1] = 0.0 * np.pi
+    model.qref = pin.neutral(model)
+    return model, geom_model
